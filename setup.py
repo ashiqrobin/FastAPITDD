7b92611f11014e9b7ab22a8ecb379437c1c50975
md.amin <md.amin@veteransunited.com>
2023-10-06 02:03:01 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='TDD',
+    version='0.0.1',
+    description='Test Driven Development',
+    author='Robin',
+    packages=find_packages(),
+)
